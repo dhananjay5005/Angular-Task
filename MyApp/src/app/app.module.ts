196fb131cafed950ea30a1150219d44786f10c61
dhananjay5005 <89375897+dhananjay5005@users.noreply.github.com>
2023-04-28 16:45:10 +0530

    First commit

---
+++ MyApp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { HeaderComponent } from './header/header.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserRegistrationComponent,
+    UserInfoComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ MyApp/src/app/user-registration/user-registration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-registration',
+  templateUrl: './user-registration.component.html',
+  styleUrls: ['./user-registration.component.css']
+  
+})
+export class UserRegistrationComponent {
+
+}
